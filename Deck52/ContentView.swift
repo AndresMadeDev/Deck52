7b3b534bfa10d0152a379cae2d718766030f0e89
AndresMadeDev <andres.made.dev@gmail.com>
2021-05-10 20:51:38 -0400

    Initial Commit

---
+++ Deck52/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Deck52
+//
+//  Created by Andres Made on 5/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Deck52/Deck52App.swift
@@ -0,0 +1,17 @@
+//
+//  Deck52App.swift
+//  Deck52
+//
+//  Created by Andres Made on 5/10/21.
+//
+
+import SwiftUI
+
+@main
+struct Deck52App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
